73fd40cd0080f63eb1bbd73ff4f9c29bbfc58ecc
Helene-mb <bunel.h85@outlook.fr>
2020-10-18 17:28:24 +0200

    Installation de Mongoose et connexion à MongoDB Atlas

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://Helene-MB:WNK7Pz74chjApjQ@pojet6-openclassrooms.lgx8p.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie YOUPI !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
+
+app.use(bodyParser.json());
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+});
 
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
@@ -29,6 +46,6 @@ app.use('/api/stuff', (req, res, next) => {
       },
     ];
     res.status(200).json(stuff);
-  });
+});
 
 module.exports = app;
