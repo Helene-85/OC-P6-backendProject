05688a521a7e5ae5f08c9a00066ed7d158f076fd
Helene-mb <bunel.h85@outlook.fr>
2020-10-22 17:28:44 +0200

    Débogage connexion utilisateur

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -5,7 +5,7 @@ const User = require('../models/User');
 
 // Inscription
 exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
+  bcrypt.hash(req.body.password, 10)                                            // On appelle la fonction de hachage, on créer un nouvel utilisateur, on le sauvegarde dans la BDD
     .then(hash => {
       const user = new User({
         email: req.body.email,
@@ -20,26 +20,26 @@ exports.signup = (req, res, next) => {
 
 // Connexion
 exports.login = (req, res, next) => {
-    User.findOne({ email: sanitize(req.body.email) })
-      .then(user => {
-        if (!user) {
-          return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-        }
-        bcrypt.compare(sanitize(req.body.password), user.password)
-            .then(valid => {
-            if (!valid) {
-              return res.status(401).json({ error: 'Mot de passe incorrect !' });
-            }
-            res.status(200).json({                                                   // Si le mot de passe est validé, un nouveau token est encodé
-              userId: user._id,
-              token: jwt.sign(
-                { userId: user._id },
-                `${process.env.JWT_KEY}`,                                            // Code aléatoire de 32 caractères masqué grâce à dotenv
-                { expiresIn: '24h' }                                                 // Expiration de la session au bout de 24h
-              )
-            });
-          })
-          .catch(error => res.status(500).json({ error }));
-      })
-      .catch(error => res.status(500).json({ error }));
+  User.findOne({ email: req.body.email })                                           // On trouve l'utilisateur qui correspond à l'email unique
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur non trouvé !' });         // Message d'erreur en cas d'utilisateur non trouvé
+      }
+      bcrypt.compare(req.body.password, user.password)                              // Si ok on compare le hash avec celui de la BDD avec bcrypt
+          .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect !' });
+          }
+          res.status(200).json({                                                   // Si le mot de passe est validé, un nouveau token est encodé
+            userId: user._id,
+            token: jwt.sign(
+              { userId: user._id },
+              `${process.env.JWT_KEY}`,                                            // Code aléatoire de 32 caractères masqué grâce à dotenv
+              { expiresIn: '24h' }                                                 // Expiration de la session au bout de 24h
+            )
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ message: 'STOP' }));
 };
